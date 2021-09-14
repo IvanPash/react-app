e306408383d26abcef8ff823291d015a124b4362
Ivan Pashkov <ivan_pashkov_2012@mail.ru>
2021-09-14 03:29:22 +0300

    31. addPost callback

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import s from './MyPosts.module.css'
 import Post from './Post/Post';
 
@@ -5,10 +6,17 @@ const MyPosts = (props) => {
     
     let postsElements = props.state.map( el => (<Post comment={el.comment} name={el.name} likeCount={el.likeCount}/>))
 
+    let newPostText = React.createRef()
+
+    let AddPost = () => {
+        alert(newPostText.current.value)
+    }
+
     return (
         <div className={s.postsContainer}>
             <h2 className={s.myPosts}>Мои посты</h2>
-            <textarea rows="5" className={s.myPosts} placeholder="Напишите что-нибудь"></textarea>
+            <textarea ref={newPostText} rows="5" className={s.myPosts} placeholder="Напишите что-нибудь"></textarea>
+            <button onClick={AddPost}>Опубликовать</button>
             <ul className={s.posts}>
                 {postsElements}
             </ul>
